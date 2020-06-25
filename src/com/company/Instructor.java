9f7279d8b1b2a245b8314442935a23f9ec1e0bbe
E1205760 <angus.cheney@santander.co.uk>
2020-06-25 14:27:27 +0100

    Added comments and tooltips

--- src/com/company/Instructor.java
+++ src/com/company/Instructor.java
@@ -5,18 +5,30 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 
 public class Instructor {
-    private InetAddress serverIP;
-    private int serverPort;
+    private InetAddress serverIP;       //IP address of the server
+    private int serverPort;             //port of the server
 
+    /**
+     * Constructor for the Instructor class, stores IP address and port
+     * @param serverIP - IP address of the server
+     * @param serverPort - port of the server
+     */
     public Instructor(InetAddress serverIP, int serverPort) {
         this.serverIP = serverIP;
         this.serverPort = serverPort;
     }
 
+    /**
+     * Instantiates a GUI object
+     */
     public void runInitiator(){
         GUI screen = new GUI(this);
     }
 
+    /**
+     * Method to send work to the Server
+     * @param duration - length of work in seconds
+     */
     public void sendNewWork(int duration) {
         String message = "NEWWORK," + duration;
         try {
@@ -28,6 +40,10 @@ public class Instructor {
             error.printStackTrace();
         }
     }
+
+    /**
+     * Method to send a SHUTDOWN command to the Server
+     */
     public void shutdown() {
         String message = "SHUTDOWN";
         try {
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,11 +4,13 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 public class Main {
-
+    /**
+     * Takes the arguments from the command line and creates and runs and Instructor object to send Work to the Server
+     * @param args - the Server IP address and Server Port - throws error for unknown host
+     */
     public static void main(String[] args) {
-
         if (args.length != 2) {
-            System.out.println("That's not enough arguments! Usage: Initiator <server IP> <server port>");
+            System.out.println("That's not the correct amount of arguments! \nArguments: <server IP> <server port> \nExample: 192.168.1.200 5000");
         } else {
             System.out.println("System running :)");
             InetAddress tempIP;
